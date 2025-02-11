369b9017c0c7a847a5038b5c0d95eee3cd55c925
Gare-Warr01 <warrgj@gmail.com>
2025-02-10 19:25:31 -0700

    10Feb2025

--- Misc/Misc/PraticeExams.R
+++ Misc/Misc/PraticeExams.R
@@ -1,23 +1,4 @@
-library(tidyverse)
-library(ggplot2)
+ggplot(smooth)
 
-#Step 1 
-read_csv("Exams/Exam_1/cleaned_covid_data.csv")
-covid_data <- read_csv("Exams/Exam_1/cleaned_covid_data.csv")
-
-#step 2
-covid_data %>% 
-  filter(grepl("^A", Province_State))
-A_states <- covid_data %>% 
-  filter(grepl("^A", Province_State))
-
-# step 3
-A_states %>%
-  ggplot(aes(x = Date, y= Deaths))
-  ggplot(aes(x = Date, y = Deaths)) +
-  geom_point() + 
-  geom_smooth(method = "loess", se = FALSE) +
-  facet_wrap(~Province_State, scales = "free") +
-  labs(title = "Deaths Over Time by State", x = "Date", y = "Deaths")
-
-
+ggplot()
+?facet_wrap
